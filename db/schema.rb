2e9a60cc21d14344436ad47016c5713f3d26c940
Henrique Waxman <henriquewsouza@gmail.com>
2023-05-29 15:36:17 -0300

    login and signup flow

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_29_164618) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_29_172849) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "orders", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "part_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["part_id"], name: "index_orders_on_part_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
+  create_table "parts", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "category"
+    t.string "origin"
+    t.integer "years"
+    t.string "name"
+    t.text "description"
+    t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_parts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,8 +44,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_29_164618) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "planet"
+    t.integer "age"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "orders", "parts"
+  add_foreign_key "orders", "users"
+  add_foreign_key "parts", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# seeds.rb
+10.times do |i|
+    User.create!(
+      email: Faker::Internet.unique.email,
+      password: 'securepassword123',
+      name: Faker::Name.name,
+      planet: ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune'][i % 8],
+      age: rand(100..500) # assuming these are alien ages
+    )
+  end
+  
+  categories = ["Engine", "Navigation System", "Life Support", "Propulsion", "Hull", "Weapon System", "Shield", "Communications"]
+  
+  10.times do |i|
+    Part.create!(
+      user_id: i + 1, # assuming users were created in order with ids 1-10
+      category: categories[i % categories.length],
+      origin: ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune'][i % 8],
+      years: rand(1..100),
+      name: Faker::Space.unique.star,
+      description: Faker::Lorem.sentence,
+      price: rand(1000..5000)
+    )
+  end
+  
