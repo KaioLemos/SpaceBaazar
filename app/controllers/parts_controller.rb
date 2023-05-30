cccf5348b193916751ede55d3961b00f658008f6
fernanda suzukawa <fernandasuzukawa@hotmail.com>
2023-05-30 14:58:42 -0300

    edit prototype

--- app/controllers/parts_controller.rb
+++ app/controllers/parts_controller.rb
@@ -1,35 +1,51 @@
 class PartsController < ApplicationController
-    before_action :set_part, only: [:show, :edit, :update, :destroy]
-    
-    def new
-        @part = Part.new
+  before_action :set_part, only: [:show, :edit, :update, :destroy]
+
+  def new
+    @part = Part.new
+  end
+
+  def create
+    @part = Part.new(part_params)
+    @part.user = current_user
+    if @part.save
+      flash[:success] = "Yay! 🎉 you successfully created a part"
+      redirect_to root_path
+    else
+      render :new
     end
-    def create
-        @part = Part.new(part_params)
-        @part.user = current_user
-        if @part.save
-            flash[:success] = "Yay! 🎉 you successfully created a part"
-            redirect_to root_path
-        else
-            render :new
-        end
+  end
+
+  def index
+    @parts = Part.all
+  end
+
+  def show
+  end
+
+  def edit
+    authorize @part
+  end
+
+  def update
+    authorize @part
+    if @part.update(part_params)
+      redirect_to @part, notice: "Part was successfully updated"
+    else
+      render :edit, status: :unprocessable_entity
     end
-    def index
-        @parts = Part.all
-    end
-    def show
-    end
-    def edit
-    end
-    def update
-    end
-    def destroy
-    end
-    private
-    def part_params
-        params.require(:part).permit(:name, :category, :origin, :years, :description, :price, :photo)
-    end
-    def set_part
-        @part = Part.find(params[:id])
-    end
+  end
+
+  def destroy
+  end
+
+  private
+
+  def part_params
+    params.require(:part).permit(:name, :category, :origin, :years, :description, :price, :photo)
+  end
+
+  def set_part
+    @part = Part.find(params[:id])
+  end
 end
