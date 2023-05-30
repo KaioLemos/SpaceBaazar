0cb02daffd972fe90b4899e83da5015b9537b863
Henrique Waxman <henriquewsouza@gmail.com>
2023-05-30 20:53:22 -0300

    new routes, controller and validations for order model

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   resources :parts
+  resources :orders, only: [:index, :new, :create, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
