0ec9574ac1494dd78cfe55538b221d52ee30169b
Rudney Santos <rudney.a.s@hotmail.com>
2023-05-30 13:24:28 -0300

    atualizacao

--- app/controllers/parts_controller.rb
+++ app/controllers/parts_controller.rb
@@ -3,6 +3,7 @@ class PartsController < ApplicationController
     def new
         @part = Part.new
     end
+
     def create
         @part = Part.new(part_params)
         @part.user = current_user
@@ -12,21 +13,28 @@ class PartsController < ApplicationController
             render :new
         end
     end
+
     def index
         @parts = Part.all
     end
+
     def show
     end
+
     def edit
     end
+
     def update
     end
+
     def destroy
     end
+
     private
     def part_params
         params.require(:part).permit(:name, :category, :origin, :years, :description, :price)
     end
+
     def set_part
         @part = Part.find(params[:id])
     end
