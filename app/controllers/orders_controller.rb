07ff3cdc1f22fd29403b4dcb30331e59d85ac789
fernanda suzukawa <fernandasuzukawa@hotmail.com>
2023-06-01 18:52:07 -0300

    lnfsdf

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,16 +2,22 @@ class OrdersController < ApplicationController
     def index
         @orders = current_user.orders
     end
+
+    def new
+      @order = Order.new(order_params)
+      @part = @order.part
+    end
+
     def create
-        @order = Order.new(order_params)
-        @part = @order.part
-        if @order.save
-            @part.completed = true
-            @part.save
-            redirect_to orders_path
-        else
-            render :new
-        end
+      @order = Order.new(order_params)
+      @part = @order.part
+      if @order.save && @order.user.where.not(user: current_user)
+        @part.completed = true
+        @part.save
+        redirect_to orders_path
+      else
+        redirect_to part_path(@part)
+      end
     end
     def show
         @order = Order.find(params[:id])
